f962b2c0a7f0903a6e2130546538d3eed34b1322
s12333563 <s12333563@students.duvalschools.org>
2021-02-18 12:48:50 -0500

    v0.3
    Finished d4() function

--- dice_roll.py
+++ dice_roll.py
@@ -1,11 +1,21 @@
-# Dice Roll Library, Joshua Clark, February 18, 2021, 12:26PM, v0.0
+# Dice Roll Library, Joshua Clark, February 18, 2021, 12:26PM, v0.3
 
 # d4 Simulator
 def roll_d4(num_roll): # num_roll is an ARGUMENT.
     import random
+    import time
 
     rolls = 0
+    sum = 0
     while rolls <= num_roll:
         result = random.randint(1,4)
         print(f"You rolled a {result} on the d4!\n"
         rolls +=1
+        time.sleep(1)
+        sum += result
+    print(f"The total roll was {sum}.\num_roll")    
+
+
+    
+
+
